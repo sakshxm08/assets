dad9a4a183b69f0feec5f2f625c621d3555708b4
sakshxm08 <saksham2211gambhir@gmail.com>
2024-03-27 12:47:51 +0530

    fixed env variables in index.js

--- backend/routes/images.js
+++ backend/routes/images.js
@@ -6,6 +6,8 @@ const { S3Client } = require("@aws-sdk/client-s3");
 const { fromIni } = require("@aws-sdk/credential-provider-ini");
 const multerS3 = require("multer-s3");
 const Image = require("../models/Image");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Configure AWS S3
 const s3 = new S3Client({
@@ -17,7 +19,7 @@ const s3 = new S3Client({
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: process.env.AWS_BUCKET_NAME || "assets.sakshxm08.in",
+    bucket: process.env.AWS_BUCKET_NAME,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: "public-read", // Make uploaded files publicly accessible
     key: function (req, file, cb) {
@@ -31,7 +33,7 @@ router.post("/upload", upload.single("file"), async (req, res) => {
   try {
     const imageUrl = req.file.location.replace(
       /https:\/\/assets\.sakshxm08\.in\.s3\.ap-south-1\.amazonaws\.com/,
-      "http://assets.sakshxm08.in"
+      process.env.DOMAIN
     );
     const name = req.file.originalname; // Access title from request body
     const image = new Image({ url: imageUrl, name }); // Create a new Image instance
